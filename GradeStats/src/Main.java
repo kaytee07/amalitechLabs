92c093f1e7f5918353f766494d09a096e5b33500
kaytee07 <kofi.taylor07@gmail.com>
2025-04-04 05:42:03 +0000

    using the try and catch block o cat non numerical excepios

--- GradeStats/src/Main.java
+++ GradeStats/src/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+    /*
+     * accept user input of grades as string
+     * find the maximum, minimum and average grade 
+     * create a graph to represent the frequency of grades between ranges
+     */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String[] gradeInput;
@@ -11,36 +16,33 @@ public class Main {
         double averageGrade;
         int[] stats = new int[5];
 
-        //accept grades of student as input 
         System.out.print("Enter student grades (separated by space): ");
         gradeInput = scanner.nextLine().split(" ");
 
-        // use number of student to sert the size of student grade
         gradesOfStudent = new int[gradeInput.length];
         
-        /*
-         * type cast student grade stored as string to integer useing the integer wrapper
-         * store the integers in the grades of studentarray
-         * set maxGrade
-         * set minGrade
-         */
+        
         for(int i = 0; i < gradeInput.length; i++){
-            System.out.print("Enter grade of a student: ");
-            gradesOfStudent[i] = Integer.parseInt(gradeInput[i]);
-            sumGradesOfStudent += gradesOfStudent[i];
+            try{
+                 System.out.print("Enter grade of a student: ");
+                gradesOfStudent[i] = Integer.parseInt(gradeInput[i]);
+                sumGradesOfStudent += gradesOfStudent[i];
 
-            if(gradesOfStudent[i] > maxGrade){
-                maxGrade = gradesOfStudent[i];
-            }
+                if(gradesOfStudent[i] > maxGrade){
+                    maxGrade = gradesOfStudent[i];
+                }
 
-            if (gradesOfStudent[i] < minGrade){
-                minGrade = gradesOfStudent[i];
-            }
+                if (gradesOfStudent[i] < minGrade){
+                    minGrade = gradesOfStudent[i];
+                }
+            }      catch (NumberFormatException e) {
+                    System.err.println("Error: '" + gradeInput[i] + "' is not a valid integer. Skipping...");
+                }
+           
 
         }
         System.out.println("");
 
-        //compute the average grade of student using type casting and math operators
         averageGrade = (double) sumGradesOfStudent / gradesOfStudent.length;
 
         System.out.println("Values: " + gradesOfStudent.length);
@@ -53,9 +55,6 @@ public class Main {
         System.out.println("");
         System.out.println("");
 
-        /*
-         * set the maximum number of student belonging too a particular gradeRange in the stats array
-         */
         for (int grades: gradesOfStudent){
             if (grades > 80){
                 stats[4]++;
@@ -70,7 +69,6 @@ public class Main {
             }
         }
 
-        // find the range with the bmaximum number of student 
         int maxNumOfStudentInRange = 0;
 
         for (int studentInRange: stats){
@@ -79,9 +77,7 @@ public class Main {
             }
         }
 
-        /*
-         * we're using a nested for loop to generate a graph to represent the number of student in each grade range
-         */
+      
         for (int i = maxNumOfStudentInRange; i >= 1; i-- ){
             System.out.printf("%3d >", i);{
                 for(int j = 0; j < stats.length; j++){
