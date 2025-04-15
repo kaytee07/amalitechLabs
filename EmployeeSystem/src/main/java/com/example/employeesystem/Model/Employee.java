6e900eee8184f144e604e082665c20c3fad25001
unknown <kofi.taylor07@gmail.com>
2025-04-15 11:40:28 +0000

    implemented thr updateFields method

--- EmployeeSystem/src/main/java/com/example/employeesystem/Model/Employee.java
+++ EmployeeSystem/src/main/java/com/example/employeesystem/Model/Employee.java
@@ -1,17 +1,66 @@
 package com.example.employeesystem.Model;
 
+import java.util.UUID;
+
 public class Employee <T> implements Comparable<Employee<T>> {
-    private T employeeId;
-    String name;
-    String department;
-    double salary;
-    double performanceRating;
-    int yearsOfExperience;
-    boolean isActive;
+    private final T employeeId;
+    private String name;
+    private String department;
+    private double salary;
+    private double performanceRating;
+    private int yearsOfExperience;
+    private boolean isActive;
 
-    Employee(){
-
+    Employee(String name, String department, double salary,
+             double performanceRating, int yearsOfExperience){
+        this.employeeId = (T) UUID.randomUUID();
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.performanceRating = performanceRating;
+        this.yearsOfExperience = yearsOfExperience;
+        this.isActive = true;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName () { return name; }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public String getDepartment(){ return department;}
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public double getSalary(){ return salary; }
+
+    public void setPerformanceRating(double performanceRating){
+        this.performanceRating = performanceRating;
+    }
+
+    public double getPerformanceRating(){
+        return performanceRating;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience){
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public int getYearsOfExperience(){
+        return  yearsOfExperience;
+    }
+
+    public void setIsActive() {
+        isActive = !isActive;
+    }
+
+    public boolean getIsActive(){ return isActive; }
 
     @Override
     public int compareTo(Employee<T> otherEmployee) {
--- EmployeeSystem/src/main/java/com/example/employeesystem/Model/EmployeeDatabase.java
+++ EmployeeSystem/src/main/java/com/example/employeesystem/Model/EmployeeDatabase.java
@@ -18,7 +18,43 @@ public class EmployeeDatabase <T>{
     }
 
     public void updateEmployeeDetail(T employeeId, String field, Object newValue) throws UserNotFoundException{
+        Employee<T> employee = employees.get(employeeId);
+        if (employee == null){
+            throw new UserNotFoundException("User not found");
+        }
+        switch (field){
+            case "name" -> {
+                if (newValue instanceof  String){
+                    employee.setName((String) newValue);
+                }
+            }
+            case "department" -> {
+                if (newValue instanceof String){
+                    employee.setDepartment((String) newValue);
+                }
+            }
+            case "salary" -> {
+                if (newValue instanceof Double){
+                    employee.setSalary((Double) newValue);
+                }
+            }
 
+            case "performanceRating" -> {
+                if (newValue instanceof Double){
+                    employee.setPerformanceRating((Double) newValue);
+                }
+            }
+
+            case "yearsOfExperience" -> {
+                if (newValue instanceof Integer){
+                    employee.setYearsOfExperience((Integer) newValue);
+                }
+            }
+
+            case "isActive" -> {
+                if (newValue instanceof  )
+            }
+        }
 
     }
 
